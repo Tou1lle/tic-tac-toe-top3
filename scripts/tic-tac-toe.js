315dde32753300227984aa5c02ab1fc18918fb01
Tuan Ha <hatuan@email.cz>
2025-05-03 00:01:15 +0200

    Change the value of the cell with method on board
    add some functionality for cells -> check if it
    already has a value, if yes, then board does
    nothing. The inital value has to be an empty string
    to check

--- scripts/tic-tac-toe.js
+++ scripts/tic-tac-toe.js
@@ -4,22 +4,53 @@ function gameBoard() {
   const columns = 3;
   const boardSize = rows * columns;
 
+  // Create the gameboard 3x3 for TOP
   for (let i = 0; i < boardSize; i++) {
     board.push(cell());
+  }
+
+  // Return the board for rendering in UI
+  const getBoard = () => board;
+
+  // Change the mark based on index
+  // if the place is already occupied (not empty string), do nothing
+  const changeMark = (index, mark) => {
+    if (board[index].hasValue()) return;
+    board[index].addValue(mark);
+  }
+
+  // Prints the board to the console for console version
+  const printBoard = () => {
+    const boardWithValues = board.map(cell => cell.getValue());
+    console.log(boardWithValues);
+  }
+
+  return {
+    getBoard,
+    printBoard,
+    changeMark,
   }
 }
 
 function cell() {
+  // Initial value for all cells
   let value = "";
 
-  const changeValue = function(playerMark) {
+  // Change the initial value to the player mark
+  const addValue = function(playerMark) {
     value = playerMark;
+  }
+
+  // Check in the board so the value can't ba changed again
+  const hasValue = () => {
+    return value !== "";
   }
 
   const getValue = () => value;
 
   return {
-    changeValue,
+    addValue,
     getValue,
+    hasValue,
   }
 }
